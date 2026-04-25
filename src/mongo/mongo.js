0ede6d4d603bc1439919eaafdafa68c7c5a1d583
SLIME <purpleblueslime@gmail.com>
2026-04-25 18:35:17 +0530

    chore: clean up 🧼

--- src/mongo/mongo.js
+++
@@ -1,8 +0,0 @@
-import { MongoClient } from 'mongodb';
-
-const { mongo } = process.env;
-
-const mong = new MongoClient(mongo);
-mong.connect((error) => console.error(error));
-
-export default mong.db('app');
